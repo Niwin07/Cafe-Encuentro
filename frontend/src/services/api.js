c3f8274847f514c5fcc0387ed75f57b605f722fd
Niwin07 <nehuenmesiasrios@gmail.com>
2025-12-05 04:56:11 -0300

    config final

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,16 +1,14 @@
-// frontend/src/services/api.js
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api', // Tu servidor Node.js
+  baseURL: 'https://cafe-encuentro.onrender.com/api',
   headers: {
     'Content-Type': 'application/json',
   },
 });
 
-// Interceptor: Antes de cada petición, inyectamos el Token si existe
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem('token'); // Recuperamos el token guardado
+  const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
