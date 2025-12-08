20149f88f0592c77d471c626c8829715f014d255
Niwin07 <nehuenmesiasrios@gmail.com>
2025-12-08 20:55:09 -0300

    boma

--- frontend/src/pages/Destinos/VistaCafeteria.jsx
+++ frontend/src/pages/Destinos/VistaCafeteria.jsx
@@ -15,7 +15,7 @@ const VistaCafeteria = () => {
 
   // Inicializar audio
   useEffect(() => {
-    audioRef.current = new Audio('/ding.mp3');
+    audioRef.current = new Audio('/ding.wav');
     audioRef.current.volume = 1.0; 
     audioRef.current.load(); 
   }, []);
